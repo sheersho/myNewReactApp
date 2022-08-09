c9becc59f409d3025170a29d9d1de18d07809e42
Sheersho Pramanik <sheersho@Sheershos-MacBook-Air.local>
2022-08-09 10:10:20 +0530

    first commit with changes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sheersho <code>Pramanik</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sheersho.github.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact Me
         </a>
       </header>
     </div>
