f257e5487cae3145b0c3f6a735ddea540a0847f9
Sheersho Pramanik <sheersho@Sheershos-MacBook-Air.local>
2022-08-09 14:17:39 +0530

    added first template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Pricing from "./pages/pricing";
+import nav from "materialize-css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sheersho <code>Pramanik</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://sheersho.github.io/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contact Me
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <nav>
+            <h2>Sheersho Pramanik</h2>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/pricing">Pricing</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/pricing" element={<Pricing />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
